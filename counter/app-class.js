feb44af7579c8efb074cad822123cacbd9b7c4f7
Svetlana Stoycheva <s.stoycheva@gmail.com>
2022-05-04 18:40:32 +0300

    comments in counter project, css update in gallery

--- counter/app-class.js
+++ counter/app-class.js
@@ -1,43 +1,44 @@
+//here we have the same two counters created with a class instead of with constructor function
 function getElement(selection) {
-  const element = document.querySelector(selection)
+  const element = document.querySelector(selection);
   if (element) {
-    return element
+    return element;
   }
-  throw new Error(`Plase check "${selection}" selector, no such element exist`)
+  throw new Error(`Plase check "${selection}" selector, no such element exist`);
 }
 
 class Counter {
   constructor(element, value) {
-    this.counter = element
-    this.value = value
-    this.resetBtn = element.querySelector('.reset')
-    this.increaseBtn = element.querySelector('.increase')
-    this.decreaseBtn = element.querySelector('.decrease')
-    this.valueDOM = element.querySelector('.value')
-    this.valueDOM.textContent = this.value
+    this.counter = element;
+    this.value = value;
+    this.resetBtn = element.querySelector('.reset');
+    this.increaseBtn = element.querySelector('.increase');
+    this.decreaseBtn = element.querySelector('.decrease');
+    this.valueDOM = element.querySelector('.value');
+    this.valueDOM.textContent = this.value;
 
-    //   this.increaseBtn.addEventListener('click', this.increase.bind(this))
-    this.increase = this.increase.bind(this)
-    this.decrease = this.decrease.bind(this)
-    this.reset = this.reset.bind(this)
+    //bind increase, decrease, reset methods to the counter (to this), so that we can use them as call back function for the eventListener
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+    this.reset = this.reset.bind(this);
 
-    this.increaseBtn.addEventListener('click', this.increase)
-    this.decreaseBtn.addEventListener('click', this.decrease)
-    this.resetBtn.addEventListener('click', this.reset)
+    this.increaseBtn.addEventListener('click', this.increase);
+    this.decreaseBtn.addEventListener('click', this.decrease);
+    this.resetBtn.addEventListener('click', this.reset);
   }
   increase() {
-    this.value++
-    this.valueDOM.textContent = this.value
+    this.value++;
+    this.valueDOM.textContent = this.value;
   }
   decrease() {
-    this.value--
-    this.valueDOM.textContent = this.value
+    this.value--;
+    this.valueDOM.textContent = this.value;
   }
   reset() {
-    this.value = 0
-    this.valueDOM.textContent = this.value
+    this.value = 0;
+    this.valueDOM.textContent = this.value;
   }
 }
 
-const firstCounter = new Counter(getElement('.first-counter'), 100)
-const secondCounter = new Counter(getElement('.second-counter'), 200)
+const firstCounter = new Counter(getElement('.first-counter'), 100);
+const secondCounter = new Counter(getElement('.second-counter'), 200);
--- counter/app-proto.js
+++ counter/app-proto.js
@@ -1,25 +1,27 @@
-//constructor function
+//In this project we are creating two independent counters using one constructor function. It creates the counter blueprint.
+
+//constructor function that creates the blueprint for our counters.
 function Counter(element, value) {
   this.counter = element;
   this.value = value;
   this.resetBtn = element.querySelector('.reset');
   this.increaseBtn = element.querySelector('.increase');
   this.decreaseBtn = element.querySelector('.decrease');
+  //if we console.log(this.resetBtn) we will see both resetBtns for both instances. The constructor function runs once for every instance.
   this.valueDOM = element.querySelector('.value');
   this.valueDOM.textContent = this.value;
 
-  //   this.increaseBtn.addEventListener('click', this.increase.bind(this))
+  //bind all functions to the counter again, so to this.
   this.increase = this.increase.bind(this);
   this.decrease = this.decrease.bind(this);
   this.reset = this.reset.bind(this);
-
   this.increaseBtn.addEventListener('click', this.increase);
   this.decreaseBtn.addEventListener('click', this.decrease);
   this.resetBtn.addEventListener('click', this.reset);
 }
-// create functions on the prototype, so that all instances have them, but we don't make copy of them.
+
+//Set functions on the prototype, so we don't copy them in every instance
 Counter.prototype.increase = function () {
-  console.log(this);
   this.value++;
   this.valueDOM.textContent = this.value;
 };
@@ -35,10 +37,13 @@ Counter.prototype.reset = function () {
 const firstCounter = new Counter(getElement('.first-counter'), 100);
 const secondCounter = new Counter(getElement('.second-counter'), 200);
 
+//helper selection function
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
     return element;
   }
-  throw new Error(`Plase check "${selection}" selector, no such element exist`);
+  throw new Error(
+    `Please check "${selection}" selector, no such element exist`
+  );
 }
